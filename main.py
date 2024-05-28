56f178093da52b9451de8a2a839b827ba7d085da
Zerok00 <lucarellidavide5@gmail.com>
2024-05-28 20:45:01 +0200

    dock

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        if os.path.isfile("../genera_qr/static/ytqr.png"):
-            os.remove("../genera_qr/static/ytqr.png")
+        if os.path.isfile("./static/ytqr.png"):
+            os.remove("./static/ytqr.png")
         link = request.form.get("link")
         qr = qrcode.QRCode(version=1, box_size=5, border=5)
         qr.add_data(link)
         qr.make()
         img = qr.make_image(fill_color = 'black', back_color = 'white')
-        img.save("../genera_qr/static/ytqr.png")
+        img.save("./static/ytqr.png")
         return render_template("home.html")
     else:
-        if os.path.isfile("../genera_qr/static/ytqr.png"):
-            os.remove("../genera_qr/static/ytqr.png")
+        if os.path.isfile("./static/ytqr.png"):
+            os.remove("./static/ytqr.png")
         return render_template("home.html")
 
 
-app.run()
+app.run(host="0.0.0.0", port=int("5000"), debug=True)
